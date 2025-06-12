380ebd94e52768517227b33168b6bd10b4bed51e
Sandhya <ragulapadusandhya7797@gmail.com>
2025-06-12 11:58:04 -0700

    Initial commit

---
+++ src/main/java/com/sandhya/remainder_app/voice_remainder/VoiceRemainderApplication.java
@@ -0,0 +1,13 @@
+package com.sandhya.remainder_app.voice_remainder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceRemainderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceRemainderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandhya/remainder_app/voice_remainder/VoiceRemainderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandhya.remainder_app.voice_remainder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceRemainderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
